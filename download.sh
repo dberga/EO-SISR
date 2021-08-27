443f84c03f68bf1292a4af046c54380140d6ca52
pgalles <pau.galles@satellogic.com>
2021-08-27 14:18:21 +0000

    Update donwload dataset file

--- download.sh
+++ download.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TO_PATH=./Data
-wget https://image-quality-framework.s3-eu-west-1.amazonaws.com/iq-sisr-use-case/datasets/ucmerced-test-micro-ds.tar.gz -O $TO_PATH/ucmerced-test-micro-ds.tar.gz
-tar xvzf $TO_PATH/ucmerced-test-micro-ds.tar.gz -C $TO_PATH
-rm $TO_PATH/ucmerced-test-micro-ds.tar.gz
+TO_PATH=.
+wget https://image-quality-framework.s3-eu-west-1.amazonaws.com/iq-sisr-use-case/datasets/ucmerced-test-ds-pre-executed.tar.gz -O $TO_PATH/ucmerced-test-ds-pre-executed.tar.gz
+tar xvzf $TO_PATH/ucmerced-test-ds-pre-executed.tar.gz -C $TO_PATH
+rm $TO_PATH/ucmerced-test-ds-pre-executed.tar.gz
