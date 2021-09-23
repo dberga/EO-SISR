323014013901829b5c04f2f98d5727b458541b1f
David Berga <david.berga@eurecat.org>
2021-09-23 18:09:53 +0200

    minor formatting changes from original iqf rebase

--- sr.py
+++ sr.py
@@ -14,19 +14,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
     parser.add_argument(
         "--traindsinput",
-        default=os.path.join(
-            args.trainds, "test" # images
-        ),
+        default=os.path.join(args.trainds, "test"),  # images
     )  # default subforlder from task ds (ds_wrapper.data_input)
-    parser.add_argument(
-        "--valds", default=args.trainds
-    )  # ds_coco_dataset
+    parser.add_argument("--valds", default=args.trainds)  # ds_coco_dataset
     args = parser.parse_args()
     parser.add_argument(
         "--valdsinput",
-        default=os.path.join(
-            args.valds, "test" # images
-        ),
+        default=os.path.join(args.valds, "test"),  # images
     )
     args = parser.parse_args()
     train_ds = args.trainds
@@ -38,16 +32,14 @@ if __name__ == "__main__":
     # objective: read train_ds and val_ds files and apply super-resolution, save in output_path/val/generated_sr_images folder
 
     # alternative (old): copy input image paths as output image paths
-    train_ds_output=train_ds_input
-    val_ds_output=val_ds_input
+    train_ds_output = train_ds_input
+    val_ds_output = val_ds_input
 
     # MOCK SR: create generated_sr_images from copy
-    
-    '''
+    """
     train_ds_output = os.path.join(
         output_path, "train", output_images_folder
     )  # train_ds_input
-        
     # create subfolders
     if not os.path.exists(output_path):
         os.mkdir(output_path)
@@ -55,7 +47,6 @@ if __name__ == "__main__":
         os.mkdir(os.path.join(output_path, "train"))
     if not os.path.exists(train_ds_output):
         os.mkdir(train_ds_output)
-
     # generate / (mock sr=copy images)
     image_files = os.listdir(train_ds_input)
     print(image_files)
@@ -63,25 +54,22 @@ if __name__ == "__main__":
         if os.path.exists(train_ds_output + "/" + image_name):
             os.remove(train_ds_output + "/" + image_name)
         os.symlink(train_ds_input + "/" + image_name, train_ds_output + "/" + image_name)
-    '''
-    '''
+    """
+    """
     if val_ds != train_ds:
-    
         val_ds_output = os.path.join(
             output_path, "val", output_images_folder
         )  # val_ds_input
-
         if not os.path.exists(os.path.join(output_path, "val")):
             os.mkdir(os.path.join(output_path, "val"))
         if not os.path.exists(val_ds_output):
             os.mkdir(val_ds_output)
-
         image_files = os.listdir(val_ds_input)
         for idx, image_name in enumerate(image_files):
             if os.path.exists(val_ds_output + "/" + image_name):
                 os.remove(val_ds_output + "/" + image_name)
             os.symlink(val_ds_input + "/" + image_name, val_ds_output + "/" + image_name)
-    '''
+    """
     output_json = {
         "train_ds": train_ds,
         "train_ds_input": train_ds_input,
@@ -89,7 +77,7 @@ if __name__ == "__main__":
         "val_ds": val_ds,
         "val_ds_input": val_ds_input,
         "val_ds_output": val_ds_output,
-        }
+    }
 
     with open(os.path.join(output_path, "output.json"), "w") as f:
         json.dump(output_json, f)
