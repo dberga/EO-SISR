ec81f907730164c54525df8ed62b906a6320d042
David Berga <david.berga@eurecat.org>
2022-04-27 17:00:33 +0200

    added tSNE function

--- visual_comparison.py
+++ visual_comparison.py
@@ -84,7 +84,7 @@ def visual_comp(
     comparison_folder = "comparison/",
     ):
     lst_lst = [glob(fr"{os.path.join(folder,'*')}") for folder in lst_folders]
-    print(''.join(['\t\t'+label for label in lst_labels]))
+    print(''.join([label+'\t\t' for label in lst_labels]))
     
     for enu,fn in enumerate(lst_lst[0]):
         if enu>20:
@@ -124,3 +124,84 @@ def visual_comp(
             plt.savefig(os.path.join(comparison_folder,"zoomed_"+os.path.basename(fn)))
         else:
             plt.show()
+
+from iq_tool_box.quality_metrics.dataloader import Dataset
+from torch.utils.data import DataLoader
+from sklearn.manifold import TSNE
+
+def plotSNE(dataset_name="test-ds", images_folder="./Data/test-ds/test/", img_size=(232,232), savefig = False,plots_folder = "plots/"):
+    # create data loader
+    dataset = Dataset(
+        "whole", # split
+        dataset_name, # dataset name
+        images_folder, # dataset images path
+        num_crops = 1,
+        crop_size = img_size,
+        split_percent = 1.0,
+        img_size = img_size,
+    )
+    dataset.__crop__(True) # only if images are of distinct size
+    dataloader= DataLoader(
+        dataset=dataset,
+        batch_size=dataset.__len__(), # one batch only
+        shuffle=True,
+        num_workers=1,
+        pin_memory=True,
+    )
+
+    # get one tensor/array of all images
+    xbatches=[x for bix,(filename, param, x, y) in enumerate(dataloader)]
+    x_data=xbatches[0] # one batch only
+
+    # reshape each image array to 1 dimension
+    if len(x_data.shape)>=4: # RGB
+        x_data = np.reshape(x_data, [x_data.shape[0], x_data.shape[1]*x_data.shape[2]*x_data.shape[3]])
+    else: # GRAY
+        x_data = np.reshape(x_data, [x_data.shape[0], x_data.shape[1]*x_data.shape[2]])
+
+    # calculate TSNE
+    tsne = TSNE(n_components=2, verbose=1, random_state=123)
+    z = tsne.fit_transform(x_data)
+    tx = z[:, 0]
+    ty = z[:, 1]
+    tx = (tx-np.min(tx)) / (np.max(tx) - np.min(tx))
+    ty = (ty-np.min(ty)) / (np.max(ty) - np.min(ty))
+
+    # Create 2d tSNE scatter plot
+    fig = plt.scatter(tx,ty)
+    plt.xlabel("tx")
+    plt.xlabel("ty")
+    if savefig is True:
+        plt.savefig(os.path.join(plots_folder,"tSNE"+"_"+dataset_name+".png"))
+    else:
+        plt.show()
+    plt.clf()
+
+    # Create 2d tSNE scatter plot with visualization of images
+
+    width = 4000
+    height = 3000
+    max_dim = 100
+
+    # get images paths
+    images = os.listdir(images_folder)
+    for idx,img_name in enumerate(images):
+        images[idx] = os.path.join(images_folder,img_name)
+
+    # create full scatter plot
+    full_image = pil_image.new('RGBA', (width, height))
+    for img, x, y in zip(images, tx, ty):
+        tile = pil_image.open(img)
+        rs = max(1, tile.width/max_dim, tile.height/max_dim)
+        tile = tile.resize((int(tile.width/rs), int(tile.height/rs)), pil_image.ANTIALIAS)
+        full_image.paste(tile, (int((width-max_dim)*x), int((height-max_dim)*y)), mask=tile.convert('RGBA'))
+
+    plt.figure(figsize = (16,12))
+    plt.imshow(full_image)
+    plt.xlabel("tx")
+    plt.xlabel("ty")
+    if savefig is True:
+        plt.savefig(os.path.join(plots_folder,"visual_tSNE"+"_"+dataset_name+".png"))
+    else:
+        plt.show()
+    plt.clf()
